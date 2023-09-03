ef86ec77bfc3f95aed66fabe435348f6b19d5568
Ruslan.Vahner <vahner0412@gmail.com>
2023-09-03 18:31:46 +0200

    Java_Home1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {12, 45, 68, 90, 42};
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+            for (int m : array) {
+                sum += m;
+            }
+            int sumDivis = 0;
+            for (int num : array) {
+                if (num % 3 == 0 && num % 5 != 0) {
+                    sumDivis += num;
+                }
+            }
+            System.out.println("Cумма всех элементов в массиве " + sum);
+            System.out.println(sumDivis);
+        }
+    }
+}
